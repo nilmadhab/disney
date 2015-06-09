e12dc07d6463987e0cdf1c0b8674afc16617ffbd
ashmanmode <ash.manmode@gmail.com>
2015-06-09 12:36:39 +0530

    adding includes

---
+++ includes/db_conn.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL ^ E_NOTICE);
+function db_connect(){
+	$con = mysqli_connect("localhost","root","25011994","disney");
+
+	// Check connection
+	if (mysqli_connect_errno())
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	else
+		echo "Connection Successful ";
+	
+	return $con;
+}
+?>
+
