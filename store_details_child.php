6bad853d0f80e6c2d0cc623c87607c156a009275
ashmanmode <ash.manmode@gmail.com>
2015-06-09 23:48:28 +0530

    child 3to6

--- store_details_child.php
+++ store_details_child.php
@@ -24,70 +24,74 @@ if($result = mysqli_query($conn,$sql))
 {
 	while($row = mysqli_fetch_array($result))
 	{
-		echo "<br>resort type entry -> ";
-		//echo explode('entityType=resort',$row['value'])[0]."---- >".$row['short_url']."<br>";
-		$checkInDate = "2015-06-14";
-		$checkOutDate = "2015-06-20";
-		$numberOfAdults = "2";
-		$numberOfChildren = "2";
-		$accessible = "1";
-		$resort = explode('entityType=resort',$row['value'])[0];
-		$coockie = 'roomForm_jar={"checkInDate":"'.$checkInDate.'","checkOutDate":"'.$checkOutDate.'","numberOfAdults":"'.$numberOfAdults.'","numberOfChildren":"'.$numberOfChildren.'","accessible":"'.$accessible.'","resort":"'.$resort.';entityType=resort","roomTypeId":false,"components":"","cartId":"","cartItemId":""}; 
-		expires=Wed, 08-Jun-2016 13:00:42 GMT; path=/';
-
-		//echo "<br>".urldecode($coockie);
-		$url = "https://disneyworld.disney.go.com/resorts/".$row['short_url']."/rates-rooms/";
-		//echo "<br>".$url ;
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_VERBOSE, 1);
-		curl_setopt($ch, CURLOPT_COOKIE, urldecode($coockie));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$page = curl_exec($ch);
-		curl_close($ch);
-
-
-		$page = str_get_html($page);
-
-		$page = $page->getElementById("pageContainer")->getElementById("cardList");
-
-
-
-		//echo $page;
-		//echo $page;
-		$links = $page->find(".roomType");
-
-
-		foreach ($links as $value) 
+		for($cm = 3 ; $cm < 7 ; $cm++ )
 		{
-			echo " entry made ";
-			//echo $value->find("h3",0)."<br />";
+			echo "<br>resort type entry -> ";
+			//echo explode('entityType=resort',$row['value'])[0]."---- >".$row['short_url']."<br>";
+			$checkInDate = "2015-06-14";
+			$checkOutDate = "2015-06-20";
+			$numberOfAdults = "2";
+			$numberOfChildren = '"'.$cm.'"';
+			$accessible = "1";
+			$resort = explode('entityType=resort',$row['value'])[0];
+			$coockie = 'roomForm_jar={"checkInDate":"'.$checkInDate.'","checkOutDate":"'.$checkOutDate.'","numberOfAdults":"'.$numberOfAdults.'","numberOfChildren":"'.$numberOfChildren.'","accessible":"'.$accessible.'","resort":"'.$resort.';entityType=resort","roomTypeId":false,"components":"","cartId":"","cartItemId":""}; 
+			expires=Wed, 08-Jun-2016 13:00:42 GMT; path=/';
 
-			//echo $value->find(".occupancy",0)."<br />";
-
-			//echo $value->find(".integer",0)."<br />";
-
-			//echo $value->find(".roomDetails",0)."<br />";
-
-			$resort_name= $row['short_url'];
-			$max_adult = mysqli_real_escape_string($conn,$value->find(".occupancy",0)->plaintext);
-			$price = mysqli_real_escape_string($conn,$value->find(".integer",0)->plaintext);
-			$details = mysqli_real_escape_string($conn,$value->find(".roomDetails",0)->plaintext);
-			$room_title = mysqli_real_escape_string($conn,$value->find("h3",0)->plaintext);
-
-			$sql1 = "INSERT INTO `occupancy_child_2`(
-				 `resort_id`, `resort_name`, `max_adult`, `price`, `details`, `room_title`)
-			 VALUES ('$resort','$resort_name','$max_adult','$price','$details','$room_title')";
-
-			//echo $sql1."<br />";
+			//echo "<br>".urldecode($coockie);
+			$url = "https://disneyworld.disney.go.com/resorts/".$row['short_url']."/rates-rooms/";
+			//echo "<br>".$url ;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_VERBOSE, 1);
+			curl_setopt($ch, CURLOPT_COOKIE, urldecode($coockie));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$page = curl_exec($ch);
+			curl_close($ch);
 
 
-			if( mysqli_query($conn,$sql1) ) {
-				//echo "recoreds inserted sucessfully";
-			}else{
-				echo "<br>insertion failed".mysqli_error($conn)."<br>";
-				echo $sql1 ;
+			$page = str_get_html($page);
+
+			$page = $page->getElementById("pageContainer")->getElementById("cardList");
+
+
+
+			//echo $page;
+			//echo $page;
+			$links = $page->find(".roomType");
+
+
+			foreach ($links as $value) 
+			{
+				echo " entry made ";
+				//echo $value->find("h3",0)."<br />";
+
+				//echo $value->find(".occupancy",0)."<br />";
+
+				//echo $value->find(".integer",0)."<br />";
+
+				//echo $value->find(".roomDetails",0)."<br />";
+
+				$resort_name= $row['short_url'];
+				$max_adult = mysqli_real_escape_string($conn,$value->find(".occupancy",0)->plaintext);
+				$price = mysqli_real_escape_string($conn,$value->find(".integer",0)->plaintext);
+				$details = mysqli_real_escape_string($conn,$value->find(".roomDetails",0)->plaintext);
+				$room_title = mysqli_real_escape_string($conn,$value->find("h3",0)->plaintext);
+
+				$table = '`occupancy_child_'.$cm.'`';
+				$sql1 = "INSERT INTO $table (
+					 `resort_id`, `resort_name`, `max_adult`, `price`, `details`, `room_title`)
+				 VALUES ('$resort','$resort_name','$max_adult','$price','$details','$room_title')";
+
+				//echo $sql1."<br />";
+
+
+				if( mysqli_query($conn,$sql1) ) {
+					//echo "recoreds inserted sucessfully";
+				}else{
+					echo "<br>insertion failed".mysqli_error($conn)."<br>";
+					echo $sql1 ;
+				}
+				
 			}
-			
 		}
 	}
 }
