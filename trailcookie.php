5677d21ceac20b6ecb32fd360fe9d7f706d243d3
ashmanmode <ash.manmode@gmail.com>
2015-06-10 00:20:06 +0530

    trailcookie

--- trailcookie.php
+++ trailcookie.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', true);
 error_reporting(E_ALL ^ E_NOTICE);
-$coockie = 'roomForm_jar=%7B%22checkInDate%22%3A%222015-06-14%22%2C%22checkOutDate%22%3A%222015-06-20%22%2C%22numberOfAdults%22%3A%222%22%2C%22numberOfChildren%22%3A%220%22%2C%22accessible%22%3A%221%22%2C%22resort%22%3A%225%3BentityType%3Dresort%22%2C%22roomTypeId%22%3Afalse%2C%22components%22%3A%22%22%2C%22cartId%22%3A%22%22%2C%22cartItemId%22%3A%22%22%7D; expires=Thu, 09-Jun-2016 00:23:49 GMT; path=/';
+$coockie = 's_sess=%20s_cc%3Dtrue%3B%20s_wdpro_lid%3D%3B%20s_sq%3D%3B%20prevPageLoadTime%3Dwdpro%252Fwdw%252Fus%252Fen%252Ftools%252Ffinder%252Fresorts%252Fanimalkingdomvillasjambohouse%252Froomrates%252Froomonly%257C9.5%3B%20s_ppv%3D-%252C18%252C18%252C657%3B; mbox=PC#1433658394516-878992.28_03#1441619388|session#1433843387792-212324#1433845248|mboxOverrideServer#mboxedge28.tt.omtrdc.net#1433845248';
 echo "<br>".urldecode($coockie);
 $url = "https://disneyworld.disney.go.com/resorts/animal-kingdom-villas-jambo/rates-rooms/";
 echo "<br>".$url ;
