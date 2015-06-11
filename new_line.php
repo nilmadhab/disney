d64e3117a7b1fafca1620ed38fc73e893805959b
nilmadhab <nilmadhab1994@gmail.com>
2015-06-11 22:50:29 +0530

    all adults

--- new_line.php
+++ new_line.php
@@ -14,5 +14,10 @@ echo "";
 
 echo "really";
 
+INSERT INTO `occupancy_adult8`(
+				 `resort_id`, `resort_name`, `max_adult`, `price`, `details`, `room_title`)
 
+			 VALUES ('80010407','beach-club-villas','Sleeps up to 4 Adults','404 ','                                                                  Deluxe Studio                                                                 View Photos for Deluxe Studio - Opens a dialog  Views of Crescent Lake, Stormalong Bay, Disney's BoardWalk or Courtyard  
+1 Queen Bed and 1 Double-Size Sleeper SofaSleeps up to 4 Adults                            ','                                 Deluxe Studio                                ')<br />
+insertion failedYou have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '
 ?>
