0c9b27473e66288f1fd98b2557d4c0180863ea5d
ashmanmode <ash.manmode@gmail.com>
2015-06-12 16:44:53 +0530

    adult added

---
+++ fetchdata.php
@@ -0,0 +1,94 @@
+<html>
+<body>
+<form action="store_details_child.php"
+class="quickQuote inline uiPlus hotels" data-plugins=
+"[[&quot;homepageQuickQuoteInlineNge&quot;,{&quot;classQuickQuoteContainer&quot;:&quot;.qqContainerHotels&quot;,&quot;idQuickQuoteWrapper&quot;:&quot;&quot;,&quot;idQuickQuoteBottomLeft&quot;:&quot;.qqLeftCorner&quot;,&quot;idQuickQuoteBottomRight&quot;:&quot;.qqRightCorner&quot;,&quot;idQuickQuoteBottomMiddle&quot;:&quot;fieldset.childAges:before&quot;,&quot;idKidsDropdownBackground&quot;:&quot;fieldset.childAges&quot;,&quot;supportsInlineHomepageTickets&quot;:false}],[&quot;pepAddRoomForm&quot;,{&quot;fields&quot;:{&quot;checkInDate&quot;:{&quot;min&quot;:&quot;2015-06-07T00:00:00-04:00&quot;,&quot;max&quot;:&quot;2015-12-31T23:59:59-05:00&quot;},&quot;checkOutDate&quot;:{&quot;min&quot;:&quot;2015-06-08T00:00:00-04:00&quot;,&quot;max&quot;:&quot;2016-01-30T23:59:59-05:00&quot;}},&quot;constraints&quot;:{&quot;minLengthOfStay&quot;:&quot;1&quot;,&quot;maxLengthOfStay&quot;:&quot;30&quot;,&quot;defaultLengthOfStay&quot;:&quot;6&quot;,&quot;maxPartySize&quot;:&quot;12&quot;,&quot;maxNumberOfAdults&quot;:&quot;9&quot;,&quot;maxNumberOfChildren&quot;:&quot;6&quot;,&quot;resort&quot;:false},&quot;errorsSummaryBoxHtml&quot;:&quot;&lt;div class=\&quot;dynamic errorsSummaryBox\&quot; tabindex=\&quot;-1\&quot; data-plugins=\&quot;[[&amp;quot;pepPageLevelError&amp;quot;,{&amp;quot;pageErrorClass&amp;quot;:&amp;quot;.dynamic&amp;quot;}]]\&quot;&gt;\n &lt;div class=\&quot;message\&quot;&gt;&lt;span class=\&quot;icon-messaging-s\&quot;&gt;&lt;\/span&gt;&lt;div class=\&quot;errorIntro\&quot;&gt;Sorry, there are one or more errors below.&lt;\/div&gt;&lt;ul class=\&quot;errorList\&quot;&gt;&lt;li class=\&quot;childAges\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Enter each child's age at the time of travel.&lt;\/li&gt;&lt;li class=\&quot;emptyCheckInDate\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Please provide the check in date for your trip.&lt;\/li&gt;&lt;li class=\&quot;emptyCheckOutDate\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Please provide the check out date for your trip.&lt;\/li&gt;&lt;li class=\&quot;emptyNumberOfAdults\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Please provide the number of adults in your party.&lt;\/li&gt;&lt;li class=\&quot;emptyNumberOfChildren\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Please provide the number of kids in your party.&lt;\/li&gt;&lt;li class=\&quot;invalidCheckInDate\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;We cannot process the check-in date you provided.&lt;\/li&gt;&lt;li class=\&quot;invalidCheckOutDate\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;We cannot process the check-out date you provided.&lt;\/li&gt;&lt;li class=\&quot;invalidResort\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Select a Resort hotel.&lt;\/li&gt;&lt;li class=\&quot;maxLengthOfStay\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Stay booked online cannot exceed 30 nights. Please &lt;a href=\&quot;\/help\/phone\/\&quot;&gt;call us&lt;\/a&gt; to book a stay longer than 30 days.&lt;\/li&gt;&lt;li class=\&quot;maxNumberOfAdults\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;You have exceeded our maximum room capacity of 9 adults. If your party is larger than 9 adults, please &lt;a href=\&quot;\/help\/phone\/\&quot;&gt;call us&lt;\/a&gt; or book multiple rooms online.&lt;\/li&gt;&lt;li class=\&quot;maxNumberOfChildren\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;You have exceeded our maximum room capacity of 6 children. If your party is larger than 6 children, please &lt;a href=\&quot;\/help\/phone\/\&quot;&gt;call us&lt;\/a&gt; or book multiple rooms online.&lt;\/li&gt;&lt;li class=\&quot;maxPartySize\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;You have exceeded our maximum room capacity of 12 people. If your party is larger than 12 people, please &lt;a href=\&quot;\/help\/phone\/\&quot;&gt;call us&lt;\/a&gt; or book multiple rooms online.&lt;\/li&gt;&lt;li class=\&quot;minCheckInDate\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;The minimum bookable date is today plus 1 day.&lt;\/li&gt;&lt;li class=\&quot;minLengthOfStay\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Select a later check-out date. You must stay at least one night.&lt;\/li&gt;&lt;li class=\&quot;minNumberOfAdults\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;Your party must contain at least 1 adult.&lt;\/li&gt;&lt;li class=\&quot;noProductAvailable\&quot;&gt;&lt;span class=\&quot;accessibleAltText\&quot;&gt;Error: &lt;\/span&gt;No information on resorts is available for the dates specified.&lt;\/li&gt;&lt;\/ul&gt;&lt;\/div&gt;\n&lt;\/div&gt;&quot;,&quot;waitForAnalyticsTracking&quot;:true,&quot;validateOnInit&quot;:true,&quot;dateFormatOptions&quot;:{&quot;momentDateFormat&quot;:&quot;MM\/DD\/YYYY&quot;,&quot;dateValidationRegEx&quot;:&quot;(0[1-9]|1[012])[\\- \\\/.](0[1-9]|[12][0-9]|3[01])[\\- \\\/.](19|20)[0-9][0-9]&quot;},&quot;childrenBirthdays&quot;:false,&quot;noChildAges&quot;:null,&quot;showPleaseWaitOnSubmit&quot;:true,&quot;selectors&quot;:{&quot;childrenContainer&quot;:&quot;fieldset.childAges&quot;,&quot;childrenLegend&quot;:&quot;fieldset.childAges label&quot;,&quot;childrenDropdownContainer&quot;:&quot;fieldset.childAges .formInputElement&quot;,&quot;childrenDropdown&quot;:&quot;fieldset.childAges .formInputElement select&quot;}}],[&quot;pepPleaseWait&quot;,{&quot;autohook&quot;:false}]]"
+method="post">
+<div class="formInputElement short numberOfAdults"
+                            data-plugins=
+                            "[&quot;pepClientSideValidatorToggle&quot;]" id=
+                            "numberOfAdults_5573d56a1a810id-base">
+                                <label class="optional" for=
+                                "numberOfAdults_5573d56a1a810"><span class=
+                                "labelValue">Adults (18+)</span><span class=
+                                "accessibleAltText">&nbsp;</span></label> <span class=
+                                "selectBox short standardFormElement"
+                                data-a11ytext=" - Opens menu"
+                                data-appendliststyleenhancements="[]"
+                                data-extraoptionclass="[]" data-plugins=
+                                "[[&quot;pepSelectBox&quot;,{&quot;maxDisplayOptions&quot;:&quot;9&quot;}]]"
+                                data-prependliststyleenhancements="[]"
+                                data-richoptgroup="[]" data-richoptioncontents=
+                                "[]" id="numberOfAdults-wrapper"><span class=
+                                "selectBox-nojs"><select class="inputElement"
+                                id="numberOfAdults_5573d56a1a810" name=
+                                "numberOfAdults" tabindex="0">
+                                    <option label="1" value="1">
+                                        1
+                                    </option>
+
+                                    <option label="2" selected="selected"
+                                    value="2">
+                                        2
+                                    </option>
+
+                                    <option label="3" value="3">
+                                        3
+                                    </option>
+
+                                    <option label="4" value="4">
+                                        4
+                                    </option>
+
+                                    <option label="5" value="5">
+                                        5
+                                    </option>
+
+                                    <option label="6" value="6">
+                                        6
+                                    </option>
+
+                                    <option label="7" value="7">
+                                        7
+                                    </option>
+
+                                    <option label="8" value="8">
+                                        8
+                                    </option>
+
+                                    <option label="9" value="9">
+                                        9
+                                    </option>
+                                </select></span></span>
+                            </div>
+                            <div class="formInputElement short accessible">
+                                <span class="pepRichCheckbox" data-plugins=
+                                "[[&quot;pepRichCheckbox&quot;,{&quot;tabindex&quot;:&quot;0&quot;}]]">
+                                <input name="accessible" type="hidden" value=
+                                "0"><input id="accessible_5573d56a1cb90" name=
+                                "accessible" tabindex="0" type="checkbox"
+                                value="1"></span> <label class="optional" for=
+                                "accessible_5573d56a1cb90"><span class=
+                                "labelValue">Accessible Rooms</span></label>
+                            </div>
+
+    <button class="pillSubmit" data-plugins=
+    "[&quot;pepActiveStyleSupport&quot;]" id=
+    "findRoomButton_5573d56a1cdc7" name=
+    "findRoomButton" tabindex="0" type=
+    "submit"><span class=
+    "pillBase primary"><span class="gradient"><span class="buttonText">Find
+    Prices</span></span> 
+    <!--[if lt IE 9]><span class="pillRightCap"></span><![endif]--></span></button>
+    <input id="offer_5573d56a1cf7d" name="offer" type=
+    "hidden" value=""> <input id=
+    "pageLocation_5573d56a1d097" name="pageLocation"
+    type="hidden" value=
+    "https://disneyworld.disney.go.com/resorts/">
+    </form>
+
+                        
+</body>
+</html>
+
+
